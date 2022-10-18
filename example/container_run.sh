630423038d09f66285132fad6214a07e6658f2a2
ShengUei <97223740+ShengUei@users.noreply.github.com>
2022-10-18 15:47:25 +0800

    Update container_run.sh

--- example/container_run.sh
+++ example/container_run.sh
@@ -1,29 +1,42 @@
 #!/bin/bash
 
-LOGS_PATH=/home/docker
-IMAGE=my_image
+LOGS_PATH=/home/docker/albert/IDG
+IMAGE=idg
 VERSION=latest
+WAIT_TIME=5
 
-if [[ "$(docker image inspect $IMAGE:$VERSION 2> /dev/null)" == "[]" ]]; then
-    echo "$IMAGE:$VERSION not exist"
-    echo "Build image"
-    docker build -t $IMAGE:$VERSION .
+
+if [[ "$(docker image inspect $IMAGE:$VERSION 2> /dev/null)" != "[]" ]]; then
+    echo "Delete old image"
+    docker image rm $IMAGE:$VERSION
 fi
 
-if [ ! -d $LOGS_PATH/logs ]; then
-    echo "logs not exist"
-    echo "Create logs directory"
-    mkdir $LOGS_PATH/logs
+echo "Build new image"
+docker build -t $IMAGE:$VERSION .
+
+if [ -d $LOGS_PATH/logs ]; then
+    echo "Delete old logs directory"
+    rm -r -f $LOGS_PATH/logs
 fi
+
+echo "Create new logs directory"
+mkdir $LOGS_PATH/logs
+
+docker compose down
 
 echo "Run docker-compose"
-
 docker compose up -d
 
 echo "Wait tomcat starting"
 
-sleep 5
+while [ "$WAIT_TIME" != "0" ]
+do
+    echo "$WAIT_TIME s"
+    WAIT_TIME=$(($WAIT_TIME-1))
+
+    sleep 1
+done
 
 chmod -R +r $LOGS_PATH/logs
 
-echo "Done"
+echo "container_run Done"
