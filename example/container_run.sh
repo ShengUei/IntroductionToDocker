b488856bf9f9fb4da986340762b6158069b33e3d
ShengUei <97223740+ShengUei@users.noreply.github.com>
2022-11-04 10:01:38 +0800

    Update container_run.sh

--- example/container_run.sh
+++ example/container_run.sh
@@ -12,8 +12,13 @@ if [[ "$(docker image inspect $IMAGE:latest 2> /dev/null)" != "[]" ]]; then
     docker image rm $IMAGE:latest
 fi
 
+if [[ "$(docker image inspect $IMAGE:$VERSION 2> /dev/null)" != "[]" ]]; then
+    echo "Delete old image $IMAGE:$VERSION"
+    docker image rm $IMAGE:$VERSION
+fi
+
 echo "Build new image"
-docker build -t $IMAGE:$VERSION -t $IMAGE:latest .
+docker build --no-cache --build-arg USER_ID=$(id -u adp) --build-arg GROUP_ID=$(id -g adp) -t $IMAGE:$VERSION -t $IMAGE:latest .
 
 if [ ! -d $LOGS_PATH/logs ]; then
     echo "Create logs directory"
@@ -21,17 +26,17 @@ if [ ! -d $LOGS_PATH/logs ]; then
 fi
 
 echo "Run docker-compose"
-docker compose up -d
+USER_ID=$(id -u adp) GROUP_ID=$(id -g adp) docker compose up -d
 
-echo "Wait tomcat starting"
-while [ "$WAIT_TIME" != "0" ]
-do
-    echo "$WAIT_TIME s"
-    WAIT_TIME=$(($WAIT_TIME-1))
+# echo "Wait tomcat starting"
+# while [ "$WAIT_TIME" != "0" ]
+# do
+#     echo "$WAIT_TIME s"
+#     WAIT_TIME=$(($WAIT_TIME-1))
+#
+#     sleep 1
+# done
 
-    sleep 1
-done
-
-chmod -R +r $LOGS_PATH/logs
+# chmod -R +r $LOGS_PATH/logs
 
 echo "container_run Done"
